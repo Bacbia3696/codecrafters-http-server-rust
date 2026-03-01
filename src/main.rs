4a59b280678f0dfeb5cceaf365a8a09f5f18b491
dat-thanh-nguyen-gfg <dat.nguyenthanh@global-fashion-group.com>
2026-03-01 14:58:15 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
@@ -9,8 +10,10 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(mut _stream) => {
+                let client_addr = _stream.peer_addr().unwrap();
+                println!("New connection from {}", client_addr);
+                _stream.write_all(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
